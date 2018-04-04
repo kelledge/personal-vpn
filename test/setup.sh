4b6832fa19fe241248175c16b291167550c3584d
Kevin Elledge <kelledge@hurricanesi.com>
2018-04-03 21:42:50 -0500

    Update compose file; Add Dockerfile for server

--- test/setup.sh
+++ test/setup.sh
@@ -2,29 +2,24 @@
 set -e
 
 OVPN_VOLUME="ovpn-data"
+IMAGE_NAME="personal-vpn:latest"
 
 docker volume rm -f "${OVPN_VOLUME}"
 docker volume create "${OVPN_VOLUME}"
 
-# This is probably more trouble that it is worth compared to a Dockerfile
-# FIXME: Script should be added to image.
-docker run -v "${OVPN_VOLUME}":/data --name tmp busybox true
-docker cp ./learn-address.sh tmp:/data
-docker rm tmp
-
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig \
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm ${IMAGE_NAME} ovpn_genconfig \
   -c \
   -n 172.16.238.254 \
   -p "dhcp-option DOMAIN-SEARCH testing.local" \
   -e 'script-security 2' \
-  -e 'learn-address /etc/openvpn/learn-address.sh' \
+  -e 'learn-address /usr/local/bin/ovpn_learn_address' \
   -u udp://openvpn-server
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki nopass
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it ${IMAGE_NAME} ovpn_initpki nopass
 
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full optimus nopass
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full bumblebee nopass
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full ratchet nopass
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it ${IMAGE_NAME} easyrsa build-client-full optimus nopass
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it ${IMAGE_NAME} easyrsa build-client-full bumblebee nopass
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm -it ${IMAGE_NAME} easyrsa build-client-full ratchet nopass
 
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient optimus > optimus.ovpn
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient bumblebee > bumblebee.ovpn
-docker run -v $OVPN_VOLUME:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient ratchet > ratchet.ovpn
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm ${IMAGE_NAME} ovpn_getclient optimus > optimus.ovpn
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm ${IMAGE_NAME} ovpn_getclient bumblebee > bumblebee.ovpn
+docker run -v $OVPN_VOLUME:/etc/openvpn --rm ${IMAGE_NAME} ovpn_getclient ratchet > ratchet.ovpn
